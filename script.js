cf8e5443a2a036c19454070f6ca5ccdd1b0f5957
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-09 15:32:34 +0300

    Размещение формы ввода тренировки

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+let map, mapEvent;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -15,29 +16,31 @@ if (navigator.geolocation) {
 
       const { longitude } = position.coords;
       const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              autoClose: false,
-              closeOnClick: false,
-              maxWidth: 200,
-              minWidth: 100,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Пробежка')
-          .openPopup();
+      map.on('click', function (event) {
+        mapEvent = event;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+        //const { lat, lng } = mapEvent.latlng;
+        //L.marker([lat, lng])
+        //  .addTo(map)
+        //  .bindPopup(
+        //    L.popup({
+        //      autoClose: false,
+        //      closeOnClick: false,
+        //      maxWidth: 200,
+        //      minWidth: 100,
+        //      className: 'running-popup',
+        //    })
+        //  )
+        //  .setPopupContent('Пробежка')
+        //  .openPopup();
       });
     },
     function () {
@@ -45,3 +48,26 @@ if (navigator.geolocation) {
     }
   );
 }
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  inputDistance.value = inputDuration.value = inputCadence.value = '';
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        autoClose: false,
+        closeOnClick: false,
+        maxWidth: 200,
+        minWidth: 100,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Пробежка')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
